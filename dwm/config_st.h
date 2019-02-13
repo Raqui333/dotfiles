400e85132e1c28f9d9fc7618ca0baef2766e9555
UserUnavailable <klebersilva2025@gmail.com>
2019-02-13 02:12:40 -0200

    dotfile

--- dwm/config_st.h
+++ dwm/config_st.h
@@ -219,26 +219,26 @@ static uint forceselmod = ShiftMask;
  */
 static Key key[] = {
 	/* keysym           mask            string      appkey appcursor */
-	{ XK_Up,			XK_NO_MOD,		"\033[A",		 0,		-1},
-	{ XK_Down,			XK_NO_MOD,		"\033[B",		 0,		-1},
-	{ XK_Left,			XK_NO_MOD,      "\033[D",		 0,		-1},
-	{ XK_Right,			XK_NO_MOD,      "\033[C",		 0,		-1},
-	{ XK_Home,			XK_NO_MOD,      "\033[H",		 0,		-1},
-	{ XK_End,			XK_NO_MOD,      "\033[4~",		 0,		 0},
-	{ XK_Insert,		XK_NO_MOD,      "\033[4h",		-1,		 0},
-	{ XK_Delete,		XK_NO_MOD,      "\033[P",		-1,		 0},
+	{ XK_Up,            XK_NO_MOD,      "\033[A",        0,     -1},
+	{ XK_Down,          XK_NO_MOD,      "\033[B",        0,     -1},
+	{ XK_Left,          XK_NO_MOD,      "\033[D",        0,     -1},
+	{ XK_Right,         XK_NO_MOD,      "\033[C",        0,     -1},
+	{ XK_Home,          XK_NO_MOD,      "\033[H",        0,     -1},
+	{ XK_End,           XK_NO_MOD,      "\033[4~",       0,      0},
+	{ XK_Insert,        XK_NO_MOD,      "\033[4h",      -1,      0},
+	{ XK_Delete,        XK_NO_MOD,      "\033[P",       -1,      0},
 	
-	{ XK_Prior,			XK_NO_MOD,		"\033[5~",       0,		 0},
-	{ XK_Next,			XK_NO_MOD,		"\033[6~",       0,		 0},
-	{ XK_BackSpace,		XK_NO_MOD,		"\177",			 0,		 0},
+	{ XK_Prior,         XK_NO_MOD,      "\033[5~",       0,      0},
+	{ XK_Next,          XK_NO_MOD,      "\033[6~",       0,      0},
+	{ XK_BackSpace,     XK_NO_MOD,      "\177",          0,	     0},
 
-	{ XK_Up,			XK_NO_MOD,		"\033OA",		 0,		+1},
-	{ XK_Down,			XK_NO_MOD,		"\033OB",		 0,		+1},
-	{ XK_Left,			XK_NO_MOD,      "\033OD",		 0,		+1},
-	{ XK_Right,			XK_NO_MOD,      "\033OC",		 0,		+1},
-	{ XK_Home,			XK_NO_MOD,      "\033[1~",		 0,		+1},
-	{ XK_Insert,		XK_NO_MOD,      "\033[2~",		+1,		 0},
-	{ XK_Delete,		XK_NO_MOD,      "\033[3~",		+1,		 0},
+	{ XK_Up,            XK_NO_MOD,      "\033OA",        0,     +1},
+	{ XK_Down,          XK_NO_MOD,      "\033OB",        0,     +1},
+	{ XK_Left,          XK_NO_MOD,      "\033OD",        0,     +1},
+	{ XK_Right,         XK_NO_MOD,      "\033OC",        0,     +1},
+	{ XK_Home,          XK_NO_MOD,      "\033[1~",       0,     +1},
+	{ XK_Insert,        XK_NO_MOD,      "\033[2~",      +1,      0},
+	{ XK_Delete,        XK_NO_MOD,      "\033[3~",      +1,      0},
 };
 
 /*
